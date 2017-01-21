bae9ef55d565ed0ad420669997e4922061efaf79
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-01-21 14:33:36 +0200

    Add basic transitons to GameManager and UILogic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public enum GameState
 {
-	START_MENU 	= 0,
-	PLAYING 	= 1,
-	GAME_OVER 	= 2
+	NONE		= 0,
+	START_MENU 	= 1,
+	PLAYING 	= 2,
+	GAME_OVER 	= 3
 }
 
 public class GameManager : Singleton<GameManager>
 {
+	[Header("UI Views")]
+	[SerializeField]
+	private StartMenuUIView 	_startMenuUIView 	= null;
+	[SerializeField]
+	private GameUIView 			_gameUIView			= null;
+	[SerializeField]
+	private HighscoreUIView 	_highscoreUIView	= null;
 
+	private GameState 			_currentState 		= GameState.NONE;
+
+	public GameState CurrentState
+	{
+		get
+		{
+			return _currentState;
+		}
+	}
+
+	private void Awake ()
+	{
+		GoToState (GameState.START_MENU);
+	}
+
+	public void GoToState (GameState newState)
+	{
+		// Initial start of the game
+		if (_currentState == GameState.NONE &&
+			newState == GameState.START_MENU)
+		{
+			_startMenuUIView
+				.TransitionIn ()
+				.Subscribe ();
+		}
+		// Start menu play was pressed - transition to playing
+		else if (_currentState == GameState.START_MENU &&
+			newState == GameState.PLAYING)
+		{
+			_startMenuUIView
+				.TransitionOut ()
+				.Concat (_gameUIView.TransitionIn ())
+				.Subscribe ();
+		}
+
+		_currentState = newState;
+	}
 }
---
+++ Assets/Scripts/UI/UIView.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using DG.Tweening;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UIView : MonoBehaviour
+{
+	[Header("UIView")]
+
+	[SerializeField]
+	[Tooltip("How long are the transitions in seconds")]
+	[Range(0.1f, float.MaxValue)]
+	protected float _transitionDuration;
+
+	protected CanvasGroup 	_uiCanvasGroup;
+	protected Tweener		_currentTween;
+
+	protected CanvasGroup UICanvasGroup
+	{
+		get
+		{
+			if (_uiCanvasGroup == null)
+			{
+				_uiCanvasGroup = GetComponent<CanvasGroup> ();
+			}
+
+			return _uiCanvasGroup;
+		}
+	}
+
+	/// <summary>
+	/// Shows the UI interface by fading in and enables interaction
+	/// </summary>
+	/// <returns>An observable sequence that completes when transition is complete.</returns>
+	virtual public IObservable<Unit> TransitionIn ()
+	{
+		return Observable.Create<Unit> (observer => {
+			if (_currentTween != null)
+			{
+				_currentTween.Kill ();
+				_currentTween = null;
+			}
+
+			_currentTween = UICanvasGroup.DOFade (1.0f, _transitionDuration)
+				.OnStart (() => {
+					UICanvasGroup.alpha = 0.0f;
+					gameObject.SetActive (true);
+				})
+				.OnComplete (() => {
+					UICanvasGroup.interactable = true;
+					observer.OnNext (Unit.Default);
+					observer.OnCompleted ();
+				});
+
+			return Disposable.Create (() => {
+				if (_currentTween != null)
+				{
+					_currentTween.Kill ();
+					_currentTween = null;
+				}
+			});
+		});
+	}
+
+	/// <summary>
+	/// Hides the UI interface by fading out and disables interaction
+	/// </summary>
+	/// <returns>An observable sequence that completes when transition is complete.</returns>
+	virtual public IObservable<Unit> TransitionOut ()
+	{
+		return Observable.Create<Unit> (observer => {
+			_currentTween = UICanvasGroup.DOFade (0.0f, _transitionDuration)
+				.OnStart (() => {
+					UICanvasGroup.interactable = false;
+				})
+				.OnComplete (() => {
+					gameObject.gameObject.SetActive (false);
+					observer.OnNext (Unit.Default);
+					observer.OnCompleted ();
+				});
+
+			return Disposable.Create (() => {
+				if (_currentTween != null)
+				{
+					_currentTween.Kill ();
+					_currentTween = null;
+				}
+			});
+		});
+	}
+}
