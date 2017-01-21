967208b811f5ab6a88f699bbcb2901bf85543056
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-01-22 01:12:10 +0200

    Add enemy explosions and fireworks to highscore

--- Assets/Scripts/UI/HighscoreUIView.cs
+++ Assets/Scripts/UI/HighscoreUIView.cs
@@ -8,11 +8,18 @@ public class HighscoreUIView : UIView
 	[Header("Highscore View")]
 	[SerializeField]
 	private Button _continueButton;
+	[SerializeField]
+	private Text _scoreText;
 
 	private void Awake ()
 	{
 		_continueButton.onClick.AddListener (() => {
 			GameManager.Instance.GoToState (GameState.START_MENU);
 		});
+	}
+
+	public void SetScore (int score)
+	{
+		_scoreText.text = string.Format ("{0} PTS", score);
 	}
 }
