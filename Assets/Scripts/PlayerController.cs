c5dfe2e37c7c3cf6de83800147da204bd1cbf943
Olli Niskanen <olli.niskanen@gmail.com>
2017-01-21 14:21:05 +0200

    Added pitch detection. Added voice controlled shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,6 +11,14 @@ public class PlayerController : MonoBehaviour {
     public GameObject cannonballPrefab;
 
     public Transform cannonballSpawn;
+
+    private void Start()
+    {
+        VoiceController.Instance.AboveThresholdStream.Throttle(System.TimeSpan.FromMilliseconds(100)).Subscribe(voiceEvent =>
+            {
+                Fire(voiceEvent);
+            });
+    }
 
 	void Update () 
     {
@@ -30,12 +39,22 @@ public class PlayerController : MonoBehaviour {
         Fire(Random.Range(0f, 90f), Random.Range(5f, 30f));
     }
 
-    public void Fire(float angle, float power)
+    private void Fire(VoiceController.VoiceEvent voiceEvent)
+    {
+        float minFreq = 200f;
+        float maxFreq = 700f;
+//        Debug.Log(string.Format("freq: {0}, vol: {1}", voiceEvent.frequency, voiceEvent.volume));
+        float clamped = Mathf.Clamp(voiceEvent.frequency, 200f, 700f);
+
+        Fire((clamped - 200f), 100000f*voiceEvent.volume);
+    }
+
+    private void Fire(float angle, float power)
     {        
-        Debug.Log(string.Format("Shooting with angle {0}, power {1}", angle, power));
+//        Debug.Log(string.Format("Shooting with angle {0}, power {1}", angle, power));
         GameObject cannonball = Instantiate(cannonballPrefab, cannonballSpawn.position, cannonballSpawn.transform.rotation);
         Vector3 force = new Vector3(0f, power * Mathf.Sin(Mathf.Deg2Rad*angle), power * Mathf.Cos(Mathf.Deg2Rad*angle));
-        Debug.Log(force);
+//        Debug.Log(force);
         cannonball.GetComponent<Rigidbody>().AddRelativeForce(force, ForceMode.Impulse);
     }
 }
